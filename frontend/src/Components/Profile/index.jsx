4b2dcf47424c99b5f1e8b2c05b549d70aa3cb8e1
radkam2000 <radkam2000@protonmail.com>
2023-06-14 19:32:28 +0200

    .

--- frontend/src/Components/Profile/index.jsx
+++ frontend/src/Components/Profile/index.jsx
@@ -2,13 +2,55 @@ import { Accordion } from "flowbite-react";
 import { ChangePasswd, DeleteAccount } from "../Forms";
 import { AccordionTitle } from "flowbite-react/lib/esm/components/Accordion/AccordionTitle";
 import { AccordionPanel } from "flowbite-react/lib/esm/components/Accordion/AccordionPanel";
+import { useState, useRef } from "react";
+import axios from "axios";
 
 const Profile = (props) => {
 	const user = props.data;
 
+	const inputFile = useRef(null);
+	const chooseFile = async () => {
+		inputFile.current.click();
+	};
+
+	const handleImport = async (file) => {
+		const token = localStorage.getItem("token");
+		if (token) {
+			try {
+				const formData = new FormData();
+				formData.append("file", file);
+				const config = {
+					method: "post",
+					url: "http://localhost:5000/api/upload/",
+					headers: {
+						"Content-Type": "multipart/form-data",
+						"x-access-token": token,
+					},
+					data: formData,
+				};
+				const { data: res } = await axios(config);
+			} catch (error) {
+				if (
+					error.response &&
+					error.response.status >= 400 &&
+					error.response.status <= 500
+				) {
+					window.location.reload();
+				}
+			}
+		}
+	};
+
 	return (
 		<div>
 			<Accordion>
+				<Accordion.Panel>
+					<Accordion.Title>Profile data</Accordion.Title>
+					<Accordion.Content>
+						<button onClick={chooseFile}>Importuj</button>
+					</Accordion.Content>
+				</Accordion.Panel>
+
 				<Accordion.Panel>
 					<Accordion.Title>Profile data</Accordion.Title>
 					<Accordion.Content>
